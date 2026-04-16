f4c79d874295907ddd10a8dbe7026dd4392ffbe6
Tamandani Ngalauka <tamandaningalauka.123929@marwadiuniversity.ac.in>
2026-04-16 23:26:52 +0530

    Fix cors

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 require("dotenv").config();
+
+const cors = require("cors");
+
+app.use(cors({
+  origin: "*",   // quick fix for submission
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", cors());
 
 const studentRoutes = require("./routes/studentRoutes");
 const authRoutes = require("./routes/authRoutes");
@@ -22,7 +31,9 @@ app.get("/", (req, res) => {
 // ======================
 // MIDDLEWARE
 // ======================
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "https://tamandani-dorothy-ngalauka.github.io/Learn.co/",
+   credentials: true
+ }));
 app.use(express.json());
 
 // ======================
@@ -53,8 +64,5 @@ mongoose.connect(process.env.MONGO_URI)
 // ======================
 // SERVER START
 // ======================
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
