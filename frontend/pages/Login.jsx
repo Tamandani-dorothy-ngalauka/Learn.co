b9e07b5b080531552616b227f41d050ad62cadbd
Tamandani Ngalauka <tamandaningalauka.123929@marwadiuniversity.ac.in>
2026-04-17 02:08:51 +0530

    fix css

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+
 export default function Login() {
   const [formData, setFormData] = useState({
     email: "",
